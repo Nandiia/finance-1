ebdbd10b86eea447c88f2b0bfa5f490ff0096eb6
Nandiia <nandin.enkhdalai@gmail.com>
2025-03-25 18:52:41 -0700

    addEventListener

---
+++ app.js
@@ -0,0 +1,39 @@
+//Дэлгэцтэй ажиллах контроллор
+
+var uiController = (function () {
+
+})();
+
+//Санхүүтэй ажиллах контроллор
+var financeController = (function () {
+
+})();
+
+
+//Холбогч контроллор
+var appController = (function (uiController, financeController) {
+
+    var ctrlAddItem = function () {
+        // Оруулах өгөгдлийг дэлгэцээс олж авна.
+        console.log("дэлэгцээс өгөгдөл авлаа")
+        // Олж авсан өгөгдүүдээ санхүүгийн контроллорт дамжуулж, тэнд хадгална.
+        // Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт гаргана
+        // Төсвийг тооцоолно
+        // Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана
+
+    };
+
+    document.querySelector(".add__btn").addEventListener("click" , function () {
+       
+
+        ctrlAddItem();
+    });
+
+    document.addEventListener("keypress" , function (event) {
+       if (event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+
+       } 
+    })
+
+})(uiController, financeController);
