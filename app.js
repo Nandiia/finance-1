90f1861b3efd90429f39749e40e7d756a0e547ea
Nandiia <nandin.enkhdalai@gmail.com>
2025-04-10 22:44:22 -0700

    showing income and expence on the desctop, clear the fields

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var uiController = (function () {
 
     var DOMstrings = {
-        iputType: ".add__type",
+        inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
         addBtn: ".add__btn"
@@ -12,7 +12,7 @@ var uiController = (function () {
     return {
         getInput: function () {
             return  {
-                type: document.querySelector(DOMstrings.iputType).value,
+                type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
@@ -20,7 +20,48 @@ var uiController = (function () {
 
         getDOMstrings: function () {
             return DOMstrings;
-        }
+        },
+
+        addListItem: function (item, type) {
+            //Орлого зарлагын элементийг агуулсан HTML-г бэлтгэнэ.
+            var html;
+            var list;
+
+            if( type === "inc"){
+                list = ".income__list"
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%%DESCRIPTION%%</div> <div class="right clearfix"> <div class="item__value">+ %%VALUE%%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+            } else {
+                list = ".expenses__list"
+                html = ' <div class="item clearfix" id="expense-%id%"> <div class="item__description">%%DESCRIPTION%%</div> <div class="right clearfix"> <div class="item__value">- %%VALUE%%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+            }
+            //Тэр HTML дотроо орлого зарлагын утгуудыг replace ашиглаж өөрчилж өгнө.
+
+            html = html.replace('%id%', item.id);
+            html = html.replace('%%DESCRIPTION%%', item.description);
+            html = html.replace('%%VALUE%%', item.value);
+            //Бэлтгэсэн HTML-ээ dom-руу хийж өгнө    
+            document.querySelector(list).insertAdjacentHTML("beforeend", html)   
+         },
+
+         clearField: function () {
+            var allfield = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+           
+            var allfieldArr = Array.prototype.slice.call(allfield);
+            
+
+            // for (var i = 0; i < allfieldArr.length; i++){
+            //     allfieldArr[i].value = ""
+            // };
+
+            allfieldArr.forEach(function(el){
+                el.value="";
+            });
+
+            allfieldArr[0].focus();
+
+
+
+         }
     } ;
     
    
@@ -53,6 +94,34 @@ var financeController = (function () {
         }
 
     };
+
+    return {
+        addItem : function (type, description, value) {
+
+            var item = {};
+
+            var id = 0
+
+            if(data.allItems[type].length === 0) id =1 
+            else {
+                 //жишээ нь income -ийн хамгийн сүүлийн ЭЛЕМЕНТИЙН id дээр нэгийг нэмээд id үүсгээд байна.
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            }
+            
+            if( type === "inc") {
+                item = new Income (id, description, value);
+            } else {
+                item = new Expense (id, description, value)
+            }
+
+           data.allItems[type].push(item);
+           //allItems[type].push(item);
+            //санхүүгийн контролорт нэмсэн ч эргээд дэлгэцэндээр гаргах учраас буцаах хэрэгтэй
+           return item
+        },
+
+        data
+    }
    
 
 })();
@@ -65,9 +134,19 @@ var appController = (function (uiController, financeController) {
 
     var ctrlAddItem = function () {
         // Оруулах өгөгдлийг дэлгэцээс олж авна. -  хүмүүс дэлгэцэн дээр юу бичсэнийг олж авахын тулд дэлгэцтэй ажилладаг uiController хийж өгнө гэсэн үг юм.
-       console.log(uiController.getInput());
+       var input = uiController.getInput();
+
         // Олж авсан өгөгдүүдээ санхүүгийн контроллорт дамжуулж, тэнд хадгална.
+        // financeController.addItem(input.type, input.description, input.value)
+
+            //
+            var item = financeController.addItem(input.type, input.description, input.value)
+        
+
         // Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт гаргана
+        uiController.addListItem(item, input.type);
+        uiController.clearField();
+
         // Төсвийг тооцоолно
         // Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана
 
@@ -91,11 +170,14 @@ var appController = (function (uiController, financeController) {
     };
 
     return {
-        init: setUpEventListeners()
+        init: function () {
+            setUpEventListeners();
+        } 
     }
 
  
 
 })(uiController, financeController);
 
-appController.init;
+appController.init();
+
